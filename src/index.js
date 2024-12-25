e2bdf0da5cd2730ce7b41ca1263ecb1364877a29
Hwayeeon <yoonakim320@gmail.com>
2024-12-26 00:07:02 +0700

    make discord bot with discordjs and makes a simple instruction

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, messageLink, Message } = require('discord.js');  // Use GatewayIntentBits in v14
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers, 
+    GatewayIntentBits.MessageContent, 
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (msg) =>{
+  if (msg.author.bot) {
+    return;
+  }
+
+  if (msg.content === 'hello') {
+    msg.reply(`Yo ${client.user.tag} Here! How i can help you today ${msg.author}`);
+  }
+});
+
+client.login(process.env.TOKEN);
